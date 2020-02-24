49e26a7819d7d2824d3bd5dd9c324bd269dc171a
salman <salman@salman-PC>
2020-02-24 13:29:52 +0530

    new comment added

--- Zxxxz39/src/test/java/Pages/Pageobject.java
+++ Zxxxz39/src/test/java/Pages/Pageobject.java
@@ -11,10 +11,11 @@ public class Pageobject {
 	
 	
 WebDriver driver;
+//this is new comment by parveen
 public Pageobject(WebDriver driver){
 	this.driver=driver;
 }
-@FindBy(name="username") WebElement user;
+@FindBy(name="username1") WebElement user;
 @FindBy(name="password") WebElement pass;
 @FindBy(id="tdb1") WebElement button;
 public void Gcrit(String Uname,String pwd){
